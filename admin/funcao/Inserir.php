ffe0b51128f912f98ddcb360ec2dcc9a6e8100f1
GMatheusst <matheusaugustomendesramalho@gmail.com>
2024-09-10 16:37:50 -0300

    Update Inserir.php

--- admin/funcao/Inserir.php
+++ admin/funcao/Inserir.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once("../classe/ManipularDados.php");
-if($_POST["idForm"] == "apProd"){
+if($_POST["idForm"] == "CrProd"){
 $apagar = new ManipularDados();
 $this->acessarTabela("tbproduto");
 $this->acessarCampo("nomeProduto,descProduto,quantProduto,valorProduto");
