23f045c42b29d03a97aaf299846f167198b99d9d
GMatheusst <matheusaugustomendesramalho@gmail.com>
2024-09-25 14:06:38 -0300

    Update EnviaContato.php

--- classe/EnviaContato.php
+++ classe/EnviaContato.php
@@ -1,11 +1,12 @@
 <?php
 include_once("ManipularDados.php");
 class EnviaContato extends ManipularDados{
-    public function EnviaContato($id,$nome,$email,$cidade,$estado,$assunto,$mensagem){
+    public function EnviaContato($nome,$email,$cidade,$estado,$assunto,$mensagem){
         $this->acessarTabela("tbontato");
-        $this->acessarCampo("idContato='$id',nomeContato=$nome',emailContato='$email',cidadeContato='$cidade',estadoContato='$estado',assuntoContato='$assunto',mensagemContato='$mensagem'");
+        $this->acessarCampo("nomeContato='$nome',emailContato='$email',cidadeContato='$cidade',estadoContato='$estado',assuntoContato='$assunto',mensagemContato='$mensagem'");
         $this->acessarDados("$nome,$email,$cidade,$estado,$assunto,$mensagem");
-        $this->acessarValorId("$id");
+        $this->acessarValorId("'$nome'");
+        $this->acessarCampoId("nomeContato")
         self::inserirDados();
         echo $this->status;
     }
